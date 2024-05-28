0233dfc3453d853f408d968444efa1af5916bee0
Victor <47047497+173716414@users.noreply.github.com>
2024-05-28 15:33:45 +0800

    添加feign客户端配置

--- voj-backend-gateway/src/main/java/com/victor/vojbackendgateway/VojBackendGatewayApplication.java
+++ voj-backend-gateway/src/main/java/com/victor/vojbackendgateway/VojBackendGatewayApplication.java
@@ -2,7 +2,9 @@ package com.victor.vojbackendgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class VojBackendGatewayApplication {
 
